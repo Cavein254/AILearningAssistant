e14a06a7fb8cf92d0bd03997fa41d1fd6e49d525
johnmuthua <caveomuthama@gmail.com>
2025-12-16 11:59:37 +0300

    chore: initialized backend with modules

--- frontend/ai-learning-assistant/src/App.jsx
+++ frontend/ai-learning-assistant/src/App.jsx
@@ -1,6 +1,15 @@
 import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/RegisterPage";
-import NotFoundPage from "./pages/Quizzes/NotFoundPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+import DocumentListPage from "./pages/Documents/DocumentListPage";
+import DocumentsDetailsPage from "./pages/Documents/DocumentDetailsPage";
+import FlashcardsListPage from "./pages/Flashcards/FlashCardListPage";
+import FlashcardPage from "./pages/Flashcards/FlashcardPage";
+import QuizTakePage from "./pages/Quizzes/QuizTakePage";
+import QuizResultPage from "./pages/Quizzes/QuizResultPage";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
 import {
   BrowserRouter as Router,
@@ -35,8 +44,18 @@ const App = () => {
         />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-
         <Route path="*" element={<NotFoundPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/documents" element={<DocumentListPage />} />
+          <Route path="/documents/:id" element={<DocumentsDetailsPage />} />
+          <Route path="/flashcards" element={<FlashcardsListPage />} />
+          <Route path="/documents/:id/flashcards" element={<FlashcardPage />} />
+          <Route path="/quizzes/:quizId" element={<QuizTakePage />} />
+          <Route path="/quizzes/:quizId/results" element={<QuizResultPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Routes>
     </Router>
   );
